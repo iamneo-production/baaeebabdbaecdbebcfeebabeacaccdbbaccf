501b611f50c49694326e155e86e5bf04a61eaa6d
Iamneo <techadmin@iamneo.ai>
2023-08-31 05:51:32 +0000

    Backup Commit

--- BloggingPlatform/Controllers/PostController.cs
+++ BloggingPlatform/Controllers/PostController.cs
@@ -7,10 +7,85 @@ using System.Linq;
 
 namespace BloggingPlatform.Controllers
 {
-    // Write your Controller here...
-    // Create
-    // Index
-    // Details
-    // Edit
-    // Delete
+    public class PostController : Controller
+    {
+        private static List<Post> _posts = new List<Post>();
+
+        public IRazorViewEngine ViewEngine { get; set; }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.Id = _posts.Count + 1; // Assign a simple incremental ID
+                _posts.Add(post);
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        public IActionResult Index()
+        {
+            return View(_posts);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var post = _posts.FirstOrDefault(p => p.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var post = _posts.FirstOrDefault(p => p.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Post updatedPost)
+        {
+            var existingPost = _posts.FirstOrDefault(p => p.Id == id);
+            if (existingPost == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                existingPost.Title = updatedPost.Title;
+                existingPost.Content = updatedPost.Content;
+                existingPost.Author = updatedPost.Author;
+                existingPost.PublishDate = updatedPost.PublishDate;
+                return RedirectToAction("Index");
+            }
+            return View(updatedPost);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var post = _posts.FirstOrDefault(p => p.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            _posts.Remove(post);
+            return RedirectToAction("Index");
+        }
+    }
 }
--- BloggingPlatform/Models/Post.cs
+++ BloggingPlatform/Models/Post.cs
@@ -2,5 +2,12 @@
 
 namespace BloggingPlatform.Models
 {
-    // Write your model class here...
+    public class Post
+    {
+        public int Id { get; set; } // Add an Id property for database mapping
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
 }
